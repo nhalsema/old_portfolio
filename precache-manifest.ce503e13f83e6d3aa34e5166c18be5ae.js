42f1f94ee80e0cc6dd1cf4836b23233733ae2605
Nicolette Halsema <nicolettebh@gmail.com>
2023-05-02 17:14:06 -0700

    Updates

---
+++ precache-manifest.ce503e13f83e6d3aa34e5166c18be5ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "61bef9a22059ce01d072",
+    "url": "/static/css/main.6e9810ba.chunk.css"
+  },
+  {
+    "revision": "61bef9a22059ce01d072",
+    "url": "/static/js/main.61bef9a2.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "95ebe4e46af2a91f5823",
+    "url": "/static/js/2.95ebe4e4.chunk.js"
+  },
+  {
+    "revision": "7855737d81de59457bd8",
+    "url": "/static/js/runtime~main.7855737d.js"
+  },
+  {
+    "revision": "696ade59738d632297e12db9905c7707",
+    "url": "/static/media/NeonFont.696ade59.otf"
+  },
+  {
+    "revision": "0899368ee7bce0b8640ea40e3149574d",
+    "url": "/static/media/ChampagneFont.0899368e.ttf"
+  },
+  {
+    "revision": "113285b8ef9352f2e7ec7a14a57af4ad",
+    "url": "/static/media/gold-texture-sm.113285b8.jpg"
+  },
+  {
+    "revision": "19abd403168fe7f7ecd78687fa35835b",
+    "url": "/static/media/BumerangFont.19abd403.otf"
+  },
+  {
+    "revision": "a1e4294a5ce8959e81247942d60230f5",
+    "url": "/static/media/gold-texture.a1e4294a.jpg"
+  },
+  {
+    "revision": "f40c217bcf750353326927ae2a2b4281",
+    "url": "/index.html"
+  }
+];
